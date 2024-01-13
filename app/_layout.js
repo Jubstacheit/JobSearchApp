ca2caa0cffaac24dc4d48b52f46f0f97dfd5a235
Viviane <dessaintviviane@gmail.com>
2024-01-13 03:53:51 +0100

    Refactor font loading and add loading state to Popularjobs and Welcome components

--- app/_layout.js
+++ app/_layout.js
@@ -7,10 +7,10 @@ SplashScreen.preventAutoHideAsync()
 
 const Layout = () => {
 	const [fontsLoaded] = useFonts({
-		DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
-		DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
-		DMMedium: require("../assets/fonts/DMSans-Medium.ttf")
-	})
+		'DMBold': require('../assets/fonts/DMSans-Bold.ttf'),
+		'DMRegular': require("../assets/fonts/DMSans-Regular.ttf"),
+		'DMMedium': require("../assets/fonts/DMSans-Medium.ttf")
+	});
 	const onLayoutRootView = useCallback(async () => {
 		if (fontsLoaded) {
 			await SplashScreen.hideAsync()
