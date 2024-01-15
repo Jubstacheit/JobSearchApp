ff03e37a661ba4310f3db32257460a6b8c2b32ba
Viviane <dessaintviviane@gmail.com>
2024-01-15 04:40:50 +0100

    Add .env file and update babel.config.js and PopularJobCard.jsx

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,12 +11,16 @@ import { useRouter } from 'expo-router'
 import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
+import useFetch from '../../../hook/useFetch'
+
 import styles from './popularjobs.style'
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React developer',
+    num_pages: 1
+  })
 
   return (
     <View style={styles.container}>
@@ -39,7 +43,7 @@ const Popularjobs = () => {
             </Text>
           ) : (
             <FlatList 
-              data={[1, 2, 3, 4, 5, 6]}
+              data={data}
               renderItem={({ item }) => (
                 <PopularJobCard 
                 item={item}/>
